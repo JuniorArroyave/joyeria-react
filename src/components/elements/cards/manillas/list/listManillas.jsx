914597ae55448051eb4e481ee3c7e6854f577b47
juniorArroyave <juniorarroyave2@gmail.com>
2026-01-13 16:29:15 -0500

    probando las imagenes

--- src/components/elements/cards/manillas/list/listManillas.jsx
+++ src/components/elements/cards/manillas/list/listManillas.jsx
@@ -1,36 +1,39 @@
+import { img1 } from "img/manilla-color-negro.jpg";
+
+
 const manillasList = [
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 1",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 2",
         descripcion: "Descripción de la manilla 2",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 3",
         descripcion: "Descripción de la manilla 3",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 4",
         descripcion: "Descripción de la manilla 4",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 5",
         descripcion: "Descripción de la manilla 5",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
+        imagenBase: {img1},
         titulo: "Manilla 6",
         descripcion: "Descripción de la manilla 6",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]

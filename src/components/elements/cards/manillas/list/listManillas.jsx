9401d977dfc30b8ce6a03c4e444d80597f417c58
juniorArroyave <juniorarroyave2@gmail.com>
2026-01-13 16:49:45 -0500

    probando foto 5

--- src/components/elements/cards/manillas/list/listManillas.jsx
+++ src/components/elements/cards/manillas/list/listManillas.jsx
@@ -1,39 +1,36 @@
-import { img1 } from "img/manilla-color-negro.jpg";
-
-
 const manillasList = [
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 1",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 2",
         descripcion: "Descripción de la manilla 2",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 3",
         descripcion: "Descripción de la manilla 3",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 4",
         descripcion: "Descripción de la manilla 4",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 5",
         descripcion: "Descripción de la manilla 5",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]
     },
     {
-        imagenBase: {img1},
+        imagenBase: "src/components/elements/cards/manillas/img/manilla-color",
         titulo: "Manilla 6",
         descripcion: "Descripción de la manilla 6",
         colores: ["negro", "rojo", "dorado", "lila", "verde", "azul"]

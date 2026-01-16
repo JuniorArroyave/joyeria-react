81641298008d8a98877ebc684933fd6e90c34bec
juniorArroyave <juniorarroyave2@gmail.com>
2026-01-16 13:30:31 -0500

    Realizar pedido

--- src/components/elements/cards/manillas/components/interactiveManilla.jsx
+++ src/components/elements/cards/manillas/components/interactiveManilla.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
+import { PedidoModal } from "./pedidoModal.jsx";
 
 export const ManillaCard = ({ manilla }) => {
     const [colorSeleccionado, setColorSeleccionado] = useState(
         manilla.colores[0]
     );
 
+    const [mostrarModal, setMostrarModal] = useState(false);
+
     return (
-        <div className="card">
-            <img
-                src={`${manilla.imagen}${colorSeleccionado}.jpg`}
-                alt={`${manilla.titulo} color ${colorSeleccionado}`}
-                className="card-image"
-            />
+        <>
+            <div className="card">
+                <img
+                    src={`${manilla.imagen}${colorSeleccionado}.jpg`}
+                    alt={`${manilla.titulo} color ${colorSeleccionado}`}
+                    className="card-image"
+                />
 
-            <div className="card-content">
-                <h2 className="card-title">{manilla.titulo}</h2>
-                <p className="card-description">{manilla.descripcion}</p>
+                <div className="card-content">
+                    <h2 className="card-title">{manilla.titulo}</h2>
+                    <p className="card-description">{manilla.descripcion}</p>
 
-                <div className="card-colors">
-                    <span>Colores disponibles:</span>
-                    <div className="colors">
-                        {manilla.colores.map((color, i) => (
-                            <span
-                                key={i}
-                                className={`color ${color} ${
-                                    color === colorSeleccionado ? "active" : ""
-                                }`}
-                                onClick={() => setColorSeleccionado(color)}
-                            />
-                        ))}
+                    <div className="card-colors">
+                        <span>Colores disponibles:</span>
+                        <div className="colors">
+                            {manilla.colores.map((color, i) => (
+                                <span
+                                    key={i}
+                                    className={`color ${color} ${
+                                        color === colorSeleccionado ? "active" : ""
+                                    }`}
+                                    onClick={() => setColorSeleccionado(color)}
+                                />
+                            ))}
+                        </div>
                     </div>
+                </div>
+
+                <div className="card-footer">
+                    <button
+                        className="card-button"
+                        onClick={() => setMostrarModal(true)}
+                    >
+                        Hacer Pedido
+                    </button>
                 </div>
             </div>
 
-            <div className="card-footer">
-                <button className="card-button">Hacer Pedido</button>
-            </div>
-        </div>
+            <PedidoModal
+                visible={mostrarModal}
+                onClose={() => setMostrarModal(false)}
+                manilla={manilla}
+                colorSeleccionado={colorSeleccionado}
+            />
+        </>
     );
 };
